0bc5543761daa0864d3a751e3213098ff0b9df60
Mahmudul107 <mahmudulislam378@gmail.com>
2024-06-05 09:57:22 +0600

    Unhandled rejection and uncaught exception detected

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ app.use(cors());
 
 // application routes
 app.use("/api/v1", router);
-// app.use("/api/v1", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
